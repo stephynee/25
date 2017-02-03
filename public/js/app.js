44c554a89f27c99ca8141dc23139e31074c9d70f
Stephanie <stephykins@gmail.com>
2017-02-03 11:38:56 -0500

    wrap angular code in iife

--- public/js/app.js
+++ public/js/app.js
@@ -1,87 +1,89 @@
-var app = angular.module('tally25', ['ui.router']);
+(function() {
+  angular.module('tally25', ['ui.router'])
 
-app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/');
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
 
-  $stateProvider
-    .state('tallies', {
-      url: '/',
-      templateUrl: '/views/tallies.html',
-      controller: 'tallyCtrl as ctrl'
-    });
-}]);
+    $stateProvider
+      .state('tallies', {
+        url: '/',
+        templateUrl: '/views/tallies.html',
+        controller: 'tallyCtrl as ctrl'
+      });
+  }])
 
-app.directive('tally', [function() {
-  return {
-    scope: {
-      data: '=data'
-    },
-    templateUrl: '/directives/tally.html',
-    controller: 'singleTallyCtrl as ctrl'
-  };
-}]);
+  .directive('tally', [function() {
+    return {
+      scope: {
+        data: '=data'
+      },
+      templateUrl: '/directives/tally.html',
+      controller: 'singleTallyCtrl as ctrl'
+    };
+  }])
 
-app.directive('addTally', [function() {
-  return {
-    templateUrl: '/directives/addtally.html',
-    controller: 'addTallyCtrl as ctrl'
-  };
-}]);
+  .directive('addTally', [function() {
+    return {
+      templateUrl: '/directives/addtally.html',
+      controller: 'addTallyCtrl as ctrl'
+    };
+  }])
 
-// directive controllers - move to another file at some point
+  // directive controllers - move to another file at some point
 
-app.controller('singleTallyCtrl', [function() {
-  var vm = this;
+  .controller('singleTallyCtrl', [function() {
+    var vm = this;
 
-  vm.showingInfo = {};
+    vm.showingInfo = {};
 
-  vm.showInfo = function(tallyId) {
-    vm.showingInfo[tallyId] = !vm.showingInfo[tallyId];
-  };
-}]);
+    vm.showInfo = function(tallyId) {
+      vm.showingInfo[tallyId] = !vm.showingInfo[tallyId];
+    };
+  }])
 
-app.controller('addTallyCtrl', ['$rootScope', 'tallyFactory', function($rootScope, tallyFactory) {
-  var vm = this;
+  .controller('addTallyCtrl', ['$rootScope', 'tallyFactory', function($rootScope, tallyFactory) {
+    var vm = this;
 
-  vm.show = false;
-  vm.tallyName = '';
-  vm.colors = ['color-red', 'color-yellow', 'color-green', 'color-seafoam', 'color-lightblue', 'color-blue', 'color-purple', 'color-pink', 'color-darkpink'];
-  vm.selectedColor = '';
+    vm.show = false;
+    vm.tallyName = '';
+    vm.colors = ['color-red', 'color-yellow', 'color-green', 'color-seafoam', 'color-lightblue', 'color-blue', 'color-purple', 'color-pink', 'color-darkpink'];
+    vm.selectedColor = '';
 
-  vm.openClose = function() {
-    vm.show = !vm.show;
-  };
-
-  vm.addTally = function() {
-    let data = {
-      task: vm.tallyName,
-      color: vm.selectedColor
+    vm.openClose = function() {
+      vm.show = !vm.show;
     };
 
-    tallyFactory.addTally(data).then(() => vm.openClose());
-  };
+    vm.addTally = function() {
+      let data = {
+        task: vm.tallyName,
+        color: vm.selectedColor
+      };
 
-  vm.selectColor = function(color) {
-    vm.selectedColor = color;
-  };
+      tallyFactory.addTally(data).then(() => vm.openClose());
+    };
 
-  $rootScope.$on('addTally', vm.openClose);
-}]);
+    vm.selectColor = function(color) {
+      vm.selectedColor = color;
+    };
 
-// //////////////////////////////
+    $rootScope.$on('addTally', vm.openClose);
+  }])
 
-app.controller('tallyCtrl', ['tallyFactory', function(tallyFactory) {
-  var vm = this;
+  // //////////////////////////////
 
-  tallyFactory.getTallies().then(data => {
-    vm.data = data.data;
-  });
-}]);
+  .controller('tallyCtrl', ['tallyFactory', function(tallyFactory) {
+    var vm = this;
 
-app.controller('headerCtrl', ['$rootScope', function($rootScope) {
-  var vm = this;
+    tallyFactory.getTallies().then(data => {
+      vm.data = data.data;
+    });
+  }])
 
-  vm.addTally = function() {
-    $rootScope.$broadcast('addTally');
-  };
-}]);
+  .controller('headerCtrl', ['$rootScope', function($rootScope) {
+    var vm = this;
+
+    vm.addTally = function() {
+      $rootScope.$broadcast('addTally');
+    };
+  }]);
+})();
--- public/js/factories.js
+++ public/js/factories.js
@@ -1,12 +1,15 @@
-app.factory('tallyFactory', ['$http', function($http) {
-  var factory = {
-    getTallies: function() {
-      return $http.get('/api/tallies');
-    },
-    addTally: function(data) {
-      return $http.post('/api/tallies', {data: data});
-    }
-  };
+(function() {
+  angular.module('tally25')
+  .factory('tallyFactory', ['$http', function($http) {
+    var factory = {
+      getTallies: function() {
+        return $http.get('/api/tallies');
+      },
+      addTally: function(data) {
+        return $http.post('/api/tallies', {data: data});
+      }
+    };
 
-  return factory;
-}]);
+    return factory;
+  }]);
+})();
