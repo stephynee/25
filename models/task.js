ed407a9fea9f283f388c6efca73ec3c4ed7e6396
Stephanie <stephykins@gmail.com>
2017-02-07 12:29:26 -0500

    add code to get route to create new daily tallys if none exist

--- models/task.js
+++ models/task.js
@@ -4,9 +4,11 @@ const taskSchema = new mongoose.Schema({
   task: String,
   color: String,
   tallies: [{
+    _id: false,
     date: {type: Date, default: Date.now},
     tally: {type: Number, default: 0}
-  }]
+  }],
+  user: {type: mongoose.Schema.Types.ObjectId, ref: 'Task'}
 });
 
 module.exports = mongoose.model('Task', taskSchema);
--- server.js
+++ server.js
@@ -25,46 +25,31 @@ app.use(express.static(path.join(__dirname, '/public')));
 //   console.log(newUser);
 // });
 
-var tempData = [
-  {
-    task: 'Programming',
-    tally: 10,
-    id: 1,
-    color: 'color-red'
-  },
-  {
-    task: 'Drawing',
-    tally: 2,
-    id: 2,
-    color: 'color-yellow'
-  },
-  {
-    task: 'Writing',
-    tally: 5,
-    id: 3,
-    color: 'color-green'
-  },
-  {
-    task: 'Reading',
-    tally: 1,
-    id: 4,
-    color: 'color-seafoam'
-  },
-  {
-    task: 'Studying',
-    tally: 12,
-    id: 5,
-    color: 'color-blue'
-  }
-];
-
 // routes need to be refactored
 app.get('/api/tallies', function(req, res) {
   User.findById('58989e697a2c1e2eb499cb25')
   .populate('tasks')
   .exec(function(err, user) {
     if(err) return console.log(err);
-    res.json(user.tasks);
+
+    var today = new Date().setHours(0, 0, 0, 0);
+    var taskRecentDate = user.tasks.length > 0 ? user.tasks[0].tallies[0].date.setHours(0, 0, 0, 0) : today;
+
+    if (today === taskRecentDate) {
+      return res.json(user.tasks);
+    }
+
+    // create new tallys for the day if none exist
+    Task.find({user: '58989e697a2c1e2eb499cb25'}, function(err, tasks) {
+      if(err) return console.log(err);
+
+      tasks.forEach(task => {
+        task.tallies.push({});
+        task.save();
+      });
+
+      res.json(tasks);
+    });
   });
 });
 
@@ -74,6 +59,19 @@ app.post('/api/tallies', function(req, res) {
     tallies: [{}],
     color: req.body.data.color
   };
+
+  User.findById('58989e697a2c1e2eb499cb25', function(err, user) {
+    if(err) return console.log(err);
+
+    Task.create(newTask, function(err, newTask) {
+      if(err) return console.log(err);
+      newTask.user = user;
+      newTask.save();
+      user.tasks.push(newTask);
+      user.save();
+      console.log(newTask.tallies);
+    });
+  });
   // this needs to be tested to make sure that the tally object is being created using the default settings
   res.json(newTask);
 });
